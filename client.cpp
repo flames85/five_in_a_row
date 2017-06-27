9472e93dd2c2df5338c83d10e99b2dc892c5b6cc
shaoqi <shaoqi@hyc.cn>
2017-06-27 17:02:26 +0800

    fix header lost

--- client.cpp
+++ client.cpp
@@ -7,6 +7,7 @@
 #include "piece.h"
 #include <stdlib.h>
 #include <string.h>
+#include <unistd.h>
 
 using namespace std;
 
--- server.cpp
+++ server.cpp
@@ -7,6 +7,7 @@
 #include <iostream>
 #include <stdlib.h>
 #include <string.h>
+#include <unistd.h>
 using namespace std;
 
 int main(int argc, char* argv[])
